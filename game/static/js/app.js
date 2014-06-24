9eea9a5d4e406d87a2ac48f0fc764a86a899453d
bramswier <bram.swier@gmail.com>
2014-06-24 22:39:07 +0200

    Update app.js
    - Er stond nog een document.getElementById in terwijl je jQuery gebruikt, dus hier een jQuery-selector van gemaakt.
    - Code indendation wat verbeterd.

--- game/static/js/app.js
+++ game/static/js/app.js
@@ -7,18 +7,17 @@
     
    var launcher = {
        init: function(){
-           //Selecteer aan de hand van een Id de canvas waarop getekend word
-            canvas = document.getElementById('myCanvas');
-           //Maak een nieuwe FPSMeter aan
-            //meter = new FPSMeter();
-           //Pak en defineer context van canvas
-            context = canvas.getContext('2d');
-           //Start de animatie loop
-            animate.loop();
-            //start server events
-            server.events();
-			countdown(13);
-            
+		//Selecteer aan de hand van een Id de canvas waarop getekend word
+		canvas = $('#myCanvas');
+		//Maak een nieuwe FPSMeter aan
+		//meter = new FPSMeter();
+		//Pak en defineer context van canvas
+		context = canvas.getContext('2d');
+		//Start de animatie loop
+		animate.loop();
+	 	//start server events
+		server.events();
+		countdown(13);
        }
    }
    
@@ -26,15 +25,15 @@
 	   	events: function(){
 		   	socket.on('message', function(message){	
 				switch(message.type){
-				case 'log': 
-					console.log(message.message);
-				break;
-				case 'update':
-					platforms = message.message.platforms;
-					balls = message.message.balls;
-					counter = message.message.counter;
-					streak = message.message.streak;
-				break;
+					case 'log': 
+						console.log(message.message);
+						break;
+					case 'update':
+						platforms = message.message.platforms;
+						balls = message.message.balls;
+						counter = message.message.counter;
+						streak = message.message.streak;
+						break;
 				}
 			});
 	   	}
@@ -58,18 +57,18 @@
             if(streak >= 5 && streak <= 10){
 	    		$('.achievement.a1').addClass('show');
 	    		$('.multi.a1').addClass('active');
-			} else if(streak >= 10 && streak <= 15) {
-				$('.achievement').removeClass('show');
-				$('.achievement.a2').addClass('show');
-				$('.multi.a2').addClass('active');
-			} else if(streak >= 15) {
-				$('.achievement').removeClass('show');
-				$('.achievement.a3').addClass('show');
-				$('.multi.a3').addClass('active');
-			} else {
-				$('.achievement').removeClass('show');
-				$('.multi').removeClass('active');
-			}
+	    } else if(streak >= 10 && streak <= 15) {
+			$('.achievement').removeClass('show');
+			$('.achievement.a2').addClass('show');
+			$('.multi.a2').addClass('active');
+	    } else if(streak >= 15) {
+			$('.achievement').removeClass('show');
+			$('.achievement.a3').addClass('show');
+			$('.multi.a3').addClass('active');
+	    } else {
+			$('.achievement').removeClass('show');
+			$('.multi').removeClass('active');
+	    }
             //Teken bal
             for(var i in balls){
             	 // bepaal middenpunt van object ten op zichte van het midden van het scherm
@@ -88,23 +87,21 @@
 	            context.lineWidth = 2;
 	            context.strokeStyle = '#f2f2f2';
 	            context.stroke();
-	            
-			        
             }
             
             //Loop door platforms en teken ze
             for(var i in platforms){
                  // bepaal middenpunt van platform ten op zichte van het midden van het scherm
-			      var centerX = (canvas.width / 2) + platforms[i].position.x;
-			      var centerY = (canvas.height / 2) + platforms[i].position.y;
+		var centerX = (canvas.width / 2) + platforms[i].position.x;
+		var centerY = (canvas.height / 2) + platforms[i].position.y;
 			      
-			      //Teken platform
-			      //Begin lijn
-			      context.beginPath();
-			      //Bepaal kleur
-			      context.fillStyle = '#ecf0f1';
-			      //Teken rechthoek
-			      context.fillRect(centerX - 30,centerY -4,60,8);
+		//Teken platform
+		//Begin lijn
+		 context.beginPath();
+		//Bepaal kleur
+		 context.fillStyle = '#ecf0f1';
+		 //Teken rechthoek
+		 context.fillRect(centerX - 30,centerY -4,60,8);
             }
         }
     }
@@ -112,7 +109,7 @@
    
    //Start lancher.init wanneer het device ready is
    $(document).ready(function(){
-	   launcher.init();
+	 launcher.init();
    });
    
 })();
